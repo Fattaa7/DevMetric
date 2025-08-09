c8c171642287daf40baa9d0907854e7dc004bd82
Ahmed Abd El-Fattah <ahmed.qweq@hotmail.com>
2025-08-10 00:11:51 +0300

    Adding Docker files

--- metrics/app/api/admin_routes.py
+++ metrics/app/api/admin_routes.py
@@ -8,6 +8,7 @@ from app.services.auth import get_current_user
 from app.schemas.metric_schema import MetricCreate, MetricResponse
 from app.models.enums.user_role import UserRole
 from app.crud.user_crud import get_user_metric_or_404
+from app.schemas.user_schema import UserResponse
 
 
 router = APIRouter(prefix="/admin", tags=["Admin"])
@@ -31,3 +32,47 @@ async def delete_all_metric(db: Session = Depends(get_db), user: User = Depends(
     db.commit()
     return None
 
+@router.delete("/user/all", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_all_users(db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    if user.role != UserRole.ADMIN:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access denied")
+    
+    users = db.query(User).all()
+    for u in users:
+        db.delete(u)
+    db.commit()
+    return None
+
+
+@router.delete("/user/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(id: int, db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    if user.role != UserRole.ADMIN:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access denied")
+
+    user_to_delete = db.query(User).filter(User.id == id).first()
+    if not user_to_delete:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    db.delete(user_to_delete)
+    db.commit()    
+    return None
+
+
+@router.get("/user/all", response_model=List[UserResponse], status_code=status.HTTP_200_OK)
+async def get_all_users(db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    if user.role != UserRole.ADMIN:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access denied")
+    
+    users = db.query(User).all()
+    return users
+
+
+@router.get("/user/{id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
+async def get_user_by_id(id: int = Path(..., title="The ID of the user to retrieve"), db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    if user.role != UserRole.ADMIN:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access denied")
+    
+    user_data = db.query(User).filter(User.id == id).first()
+    if not user_data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    return UserResponse.model_validate(user_data)
--- metrics/app/main.py
+++ metrics/app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from app.api import user_routes, auth_routes, metric_routes, admin_routes
 from app.core.database import Base, engine
+import os
 
+
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+db = os.getenv("POSTGRES_DB")
+host = os.getenv("POSTGRES_HOST")
+port = os.getenv("POSTGRES_PORT")
+
+DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+
+
+# Initialize the database connection
 # Create DB tables
 Base.metadata.create_all(bind=engine)
 
